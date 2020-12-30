05c7e8367a608652deb9347cf1a911afb74778c3
Andy Ho <hosyvietanh@gmail.com>
2020-12-30 13:50:00 +0700

    Fix some command issue

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -2,7 +2,6 @@ mod install;
 mod start;
 mod stop;
 mod pull;
-mod upgrade;
 mod wipe;
 
 use clap_nested::Commander;
@@ -20,8 +19,7 @@ pub fn commander<'a>() -> Commander<'a, (), ()> {
         .add_cmd(start::cmd())
         .add_cmd(stop::cmd())
         .add_cmd(pull::cmd())
-//        .add_cmd(upgrade::cmd())
-//        .add_cmd(wipe::cmd())
+        .add_cmd(wipe::cmd())
         .no_cmd(|_args, _matches| {
             println!("No subcommand matched");
             Ok(())
--- src/cmd/wipe.rs
+++ src/cmd/wipe.rs
@@ -1,20 +1,32 @@
 #![allow(dead_code)]
 use clap_nested::Command;
+use colored::Colorize;
 use duct;
 
 pub fn cmd<'a>() -> Command<'a, ()> {
     Command::new("wipe")
         .description("Stop all services and clean all block chain and bridge data")
         .runner(|_env, _matches| {
-            duct::cmd("docker-compose", vec!["down", "-v"])
+            let res = duct::cmd("docker-compose", vec!["down", "-v"])
                 .then(
                     duct::cmd("rm", vec![
                         "-r",
                         &format!("{}/.skymavis", std::env::var("HOME").unwrap()),
                     ])
                 )
-                .run()
-                .unwrap();
+                .run();
+            match res {
+                Err(e) => {
+                    println!("Error happened: {}", e);
+                    println!(
+                        "Please run this command to wipe data: {}",
+                        &format!("sudo rm -r {}/.skymavis", std::env::var("HOME").unwrap()).yellow(),
+                    );
+                }
+                _ => {
+                    println!("Data wiped successfully!");
+                }
+            }
             Ok(())
         })
 }
